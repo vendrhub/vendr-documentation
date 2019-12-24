aae612baa0fdb997aad6302b0460920557c00e1c
mattbrailsford <me@mattbrailsford.com>
2019-12-24 15:53:10 +0000

    Removed unused tag

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -7,7 +7,6 @@
         <!-- Sidebar -->
         <div id="sidebar" class="hidden fixed inset-0 border-gray-300 pt-16 h-full bg-white z-90 w-full border-b lg:-mb-0 lg:static lg:h-auto lg:overflow-y-visible lg:border-b-0 lg:pt-0 lg:w-1/4 lg:block lg:border-0 lg:border-r xl:w-1/5 ">
           <div id="navWrapper" class="h-full overflow-y-auto scrolling-touch lg:h-auto lg:block lg:relative lg:sticky lg:pt-16 lg:top-0 bg-white lg:bg-transparent">
-            <div id="navGradient" class="hidden"></div>
             <nav id="nav" class="overflow-x-hidden px-6 pt-6 overflow-y-auto lg:py-8 lg:pl-6 lg:pr-8 sticky?lg:h-(screen-16)">
               <slot name="sidebar" />              
             </nav>
