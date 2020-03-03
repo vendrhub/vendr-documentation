2109bdad011c6820313258faef103b20f7b49ef5
mattbrailsford <me@mattbrailsford.com>
2020-03-03 10:58:20 +0000

    Stack prev/next links on mobile

--- src/templates/DocPage.vue
+++ src/templates/DocPage.vue
@@ -7,9 +7,9 @@
       <edit-on-github-link :path="$page.doc.fileInfo.path" />
     </p>
 
-    <nav class="border-t-2 border-gray-200 flex w-full pt-6 mt-8" v-if="previousPage || nextPage">
-      <div class="flex-1" v-if="previousPage" :class="{ 'mr-2': nextPage }">
-        <g-link exact class="group rounded transition flex items-center w-full border border-gray-300 px-4 py-3 hover:bg-brand-blue-light hover:text-white hover:border-brand-blue-light" :to="previousPage.link">
+    <nav class="flex flex-col-reverse border-t-2 border-gray-200 items-stretch w-full pt-6 mt-8 sm:flex-row" v-if="previousPage || nextPage">
+      <div class="flex-1 sm:mt-0" v-if="previousPage" :class="{ 'mt-2': nextPage, 'sm:mr-2': nextPage }">
+        <g-link exact class="group rounded transition flex items-center w-full h-full border border-gray-300 px-4 py-3 hover:bg-brand-blue-light hover:text-white hover:border-brand-blue-light" :to="previousPage.link">
           <span class="pr-3">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="text-gray-500 w-8 h-8 stroke-current group-hover:text-white" fill="none" stroke-width="1"><g><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></g></svg>
           </span>
@@ -19,8 +19,8 @@
           </span>
         </g-link>
       </div>
-      <div class="flex-1" v-if="nextPage" :class="{ 'ml-2': previousPage }">
-        <g-link exact class="group rounded transition flex items-center w-full border border-gray-300 px-4 py-3 hover:bg-brand-blue-light hover:text-white hover:border-brand-blue-light" :to="nextPage.link">
+      <div class="flex-1 sm:mb-0" v-if="nextPage" :class="{ 'mb-2': nextPage, 'sm:ml-2': previousPage }">
+        <g-link exact class="group rounded transition flex items-center w-full h-full border border-gray-300 px-4 py-3 hover:bg-brand-blue-light hover:text-white hover:border-brand-blue-light" :to="nextPage.link">
           <span class="flex-1 text-left">
             <span class="block text-gray-500 text-xs font-bold transition group-hover:text-white">{{ nextPage.group }}</span>
             <span class="block text-md">{{ nextPage.title }}</span>
