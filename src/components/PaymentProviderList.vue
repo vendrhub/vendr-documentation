2edd1a22917abeb731b70117fa3a8fb14d0ffd52
mattbrailsford <me@mattbrailsford.com>
2020-08-11 13:48:27 +0100

    Added Buckaroo payment provider stub

--- src/components/PaymentProviderList.vue
+++ src/components/PaymentProviderList.vue
@@ -3,7 +3,7 @@
     <div class="tile-grid__cell p-3 w-full md:w-1/2 lg:w-1/3" v-for="edge in $static.paymentProviders.edges" :key="edge.node.id">
           <g-link :to="edge.node.docVersions.current.path" class="tile flex flex-col text-center items-stretch h-full relative">
             <template v-if="edge.node.logo">
-                <span class="block text-center my-6"><g-image :src="require(`!!assets-loader!@logos/${edge.node.logo}`)" :alt="edge.node.name" class="mx-auto h-12 w-auto g-image--plain" immediate="true" /></span>
+                <span class="block text-center my-6"><g-image :src="require(`!!assets-loader!@logos/${edge.node.logo}`)" :alt="edge.node.name" class="mx-auto h-12 w-auto g-image--plain" immediate="true" style="max-width: 60%;" /></span>
                 <hr class="my-6" />
             </template>
             <span class="tile__heading text-xl font-bold">{{ edge.node.name }}</span>
